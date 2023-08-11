66ba681372ef317a6700dda061d741c376c5a342
Morgan Peck <mlpeck@arkbluecross.com>
2023-08-11 16:35:35 -0500

    Starting to build out the events calendar/app

---
+++ src/components/EventCard.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+  event: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="event-card">
+    <h2>{{ event.title }}</h2>
+    <span>@{{ event.time }} on {{ event.date }}</span>
+  </div>
+</template>
+
+<style scoped>
+.event-card {
+  padding: 1.5rem;
+  width: 15rem;
+  cursor: pointer;
+  border: 0.1rem solid #39495c;
+  margin-bottom: 1rem;
+}
+
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 0.2rem 0.6rem 0 rgba(0, 0, 0, 0.2);
+}
+</style>
