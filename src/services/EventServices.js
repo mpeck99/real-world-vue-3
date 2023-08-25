0713d408371e30064861e376f0b496c7e74c3a6b
Morgan Peck <mlpeck@arkbluecross.com>
2023-08-25 10:01:31 -0500

    creating a single axios file so its easier to maintain/debug when there are multiple api calls

---
+++ src/services/EventServices.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL : 'https://my-json-server.typicode.com/mpeck99/real-world-vue-3',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+  getEvents(){
+    return apiClient.get('/events')
+  },
+  
+  getEvent(id){
+    return apiClient.get('/events/'+id)
+  }
+}
