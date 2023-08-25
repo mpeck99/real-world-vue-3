a122d0c40d1038b1e35887dfaa8d53af7420a9b8
Morgan Peck <mlpeck@arkbluecross.com>
2023-08-25 14:01:40 -0500

    starting to set up the pagination stuff

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,29 +1,41 @@
-<script setup>
-import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventServices.js'
-import { ref, onMounted } from 'vue'
-
-const events = ref(null)
-onMounted(() => {
-  EventService.getEvents()
-    .then((response) => {
-      events.value = response.data
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-})
-</script>
-
 <template>
-  <div id="events">
-    <h1>Events For Good</h1>
+  <h1>Events For Good</h1>
+  <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
+<script>
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventServices.js'
+
+export default {
+  name: 'EventListView',
+  props: ['page'],
+  components: {
+    EventCard,
+  },
+
+  data() {
+    return {
+      events: null,
+    }
+  },
+
+  created() {
+    EventService.getEvents(2, this.page)
+      .then((response) => {
+        this.events = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  },
+}
+</script> 
+
 <style scoped>
-#events {
+.events {
   display: flex;
   flex-direction: column;
   align-items: center;
